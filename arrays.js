2b025d0a730b759d8b452bf5029606e82f4e06df
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-28 13:38:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,6 @@
 var chocolateBars =["snickers", "hundred grand", "kitkat", "skittles"]
 
+function addElementToBeginningOfArray(array, element){
+  var myArray=[element,...array]
+  return myArray
+}
