bd95b03e833dc2fb31eb9fef2bdb293eb7447d56
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-28 14:59:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
   return array
 }
+
+function removeElementFromBeginningOfArray(array){
+  myArray=array.slice(1)
+  return myArray
+}
