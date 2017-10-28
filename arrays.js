424f2130539062aa58582bd02a8784091d4b9433
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-28 13:47:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,6 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array = [element, array]
+  array = [element, ...array]
   return array
 }
